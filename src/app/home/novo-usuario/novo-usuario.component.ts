bf16b2b6e1f7e92fceb2c5a908b51967eceabc78
luizfiller <luiz.fsilva@sptech.school>
2023-03-10 11:20:51 -0300

    Add - Sing up Reactive Form using interface, FormBuilder, FormGroup

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { NovoUsuarioService } from './novo-usuario.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
-export class NovoUsuarioComponent {
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private NovoUsuarioService: NovoUsuarioService
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+
+    console.log(novoUsuario);
+  }
 }
