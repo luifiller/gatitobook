123988dc69ac6088581abec513f4ac58404b76fa
luizfiller <luiz.fsilva@sptech.school>
2023-03-10 13:05:41 -0300

    Add - validators for inputs using *ngIf and a custom validator for lowerCase username

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,8 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -18,10 +19,20 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
-      password: [''],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      fullName: ['', [
+        Validators.required,
+        Validators.minLength(4)
+      ]],
+      userName: ['', [
+        minusculoValidator
+      ]],
+      password: ['', [
+        Validators.required,
+      ]],
     });
   }
 
