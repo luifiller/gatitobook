25eb677ca2f98b9640c1c1eab09abe104a3c0bff
luizfiller <luiz.fsilva@sptech.school>
2023-03-14 09:33:56 -0300

    Add - authToken to get headers HTTP with pipe + tap

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,20 +1,34 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  autenticar(usuario: string, senha: string): Observable<any> {
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
     // Observable é semelhante à 'promisse' do JavaScript. Aqui, é um objeto que quando a requisição retornar/completar irá retornar o objeto definido dentro do 'Observable'.
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha
-    })
+    return this.httpClient.post(
+      'http://localhost:3000/user/login',
+      {
+        userName: usuario,
+        password: senha,
+      },
+      // o "observe: 'response'" recolhe as infos do header da requisição
+      {
+        observe: 'response',
+      }
+    ).pipe (
+      tap((res) => {
+        const authToken = res.headers.get('x-acess-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    );
   }
-
 }
